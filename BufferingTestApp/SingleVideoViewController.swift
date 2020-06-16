a0d93c6fddb8beab9684af7ab81ced112a024ed3
Marius Seufzer <marius.seufzer@code.berlin>
2020-06-16 15:42:40 +0200

    Observe and display timeControlStatus and reasonForWaitingToPlay

--- BufferingTestApp/SingleVideoViewController.swift
+++ BufferingTestApp/SingleVideoViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import Combine
 import AVKit
 
 class SingleVideoViewController: UIViewController {
-
+    // MARK: - Declaration
     private lazy var playerContainerView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemFill
@@ -28,17 +29,26 @@ class SingleVideoViewController: UIViewController {
         return stackView
     }()
 
-    private let playerLayer: AVPlayerLayer = {
+    private lazy var playerLayer: AVPlayerLayer = {
         let avPlayerLayer = AVPlayerLayer()
 
         let url = URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!
         let playerItem = AVPlayerItem(asset: .init(url: url))
         let player = AVPlayer(playerItem: playerItem)
+        player.addObserver(self, forKeyPath: "timeControlStatus", options: [], context: nil)
+        player.addObserver(self, forKeyPath: "reasonForWaitingToPlay", options: [], context: nil)
         avPlayerLayer.player = player
 
         return avPlayerLayer
     }()
 
+    private let videoInfoView: VideoInfoView = {
+        let videoInfoView = VideoInfoView()
+        videoInfoView.translatesAutoresizingMaskIntoConstraints = false
+        return videoInfoView
+    }()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpViews()
@@ -51,6 +61,7 @@ class SingleVideoViewController: UIViewController {
         }
     }
 
+    // MARK: - Setup
     private func setUpViews() {
         view.backgroundColor = .systemBackground
         navigationItem.title = "Test single video"
@@ -75,16 +86,20 @@ class SingleVideoViewController: UIViewController {
         controlButtonStackView.addArrangedSubview(pauseButton)
         controlButtonStackView.addArrangedSubview(playButton)
         controlButtonStackView.addArrangedSubview(playImmediatelyButton)
+        view.addSubview(videoInfoView)
 
         NSLayoutConstraint.activate([
-            playerContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
+            playerContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             playerContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             playerContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            playerContainerView.heightAnchor.constraint(equalToConstant: 300),
+            playerContainerView.heightAnchor.constraint(equalToConstant: 250),
 
             controlButtonStackView.topAnchor.constraint(equalTo: playerContainerView.bottomAnchor, constant: 8),
             controlButtonStackView.leadingAnchor.constraint(equalTo: playerContainerView.leadingAnchor, constant: 8),
-            controlButtonStackView.trailingAnchor.constraint(equalTo: playerContainerView.trailingAnchor, constant: -8)
+            controlButtonStackView.trailingAnchor.constraint(equalTo: playerContainerView.trailingAnchor, constant: -8),
+
+            videoInfoView.topAnchor.constraint(equalTo: controlButtonStackView.bottomAnchor, constant: 8),
+            videoInfoView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 
@@ -99,5 +114,16 @@ class SingleVideoViewController: UIViewController {
     @objc private func didTapPlayImmediately() {
         playerLayer.player?.playImmediately(atRate: 1)
     }
-}
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let player = playerLayer.player, object as AnyObject? === player else { return }
+        switch keyPath {
+        case "timeControlStatus":
+            videoInfoView.setTimeControlStatus(player.timeControlStatus)
+        case "reasonForWaitingToPlay":
+            videoInfoView.setReasonForWaitingToPlay(player.reasonForWaitingToPlay)
+        default:
+            break
+        }
+    }
+}
---
+++ BufferingTestApp/VideoInfoView.swift
@@ -0,0 +1,93 @@
+//
+//  VideoInfoView.swift
+//  BufferingTestApp
+//
+//  Created by Marius Seufzer on 16.06.20.
+//  Copyright © 2020 Marius Seufzer. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoInfoView: UIView {
+    private let listStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private let timeControlStatusLabel = UILabel()
+    private let reasonForWaitingToPlayLabel = UILabel()
+
+    init() {
+        super.init(frame: .zero)
+
+        addSubview(listStackView)
+        NSLayoutConstraint.activate([
+            listStackView.topAnchor.constraint(equalTo: topAnchor),
+            listStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            listStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            listStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        addInfo(description: "timeControlStatus", statusLabel: timeControlStatusLabel)
+        addInfo(description: "reasonForWaitingToPlay", statusLabel: reasonForWaitingToPlayLabel)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    /// This method will add a new line to the listStackView. It creates a description label with the passed description and puts it together with the
+    /// statusLabel in a horizontal StackView.
+    private func addInfo(description: String, statusLabel: UILabel) {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = description
+        descriptionLabel.textAlignment = .right
+        descriptionLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+
+        statusLabel.font = .systemFont(ofSize: 14, weight: .regular)
+
+        stackView.addArrangedSubview(descriptionLabel)
+        stackView.addArrangedSubview(statusLabel)
+
+        listStackView.addArrangedSubview(stackView)
+    }
+
+    func setTimeControlStatus(_ timeControlStatus: AVPlayer.TimeControlStatus) {
+        switch timeControlStatus {
+        case .paused:
+            timeControlStatusLabel.text = "Paused"
+        case .playing:
+            timeControlStatusLabel.text = "Playing"
+        case .waitingToPlayAtSpecifiedRate:
+            timeControlStatusLabel.text = "Waiting"
+        @unknown default:
+            fatalError("Unknown case")
+        }
+    }
+
+    func setReasonForWaitingToPlay(_ reason: AVPlayer.WaitingReason?) {
+        switch reason {
+        case .some(.evaluatingBufferingRate):
+            reasonForWaitingToPlayLabel.text = "Evaluating buffering rate"
+        case .some(.noItemToPlay):
+            reasonForWaitingToPlayLabel.text = "Not item to play"
+        case .some(.toMinimizeStalls):
+            reasonForWaitingToPlayLabel.text = "Minimizing Stalls"
+        case .none:
+            reasonForWaitingToPlayLabel.text = ""
+        default:
+            fatalError("Unexpected case")
+        }
+    }
+}
